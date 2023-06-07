259d3cc237af434997b0b12b3654ad69d12a8c0c
Anil Pandiya <anilpandiya05@gmail.com>
2023-06-07 16:43:51 +0530

    FE | chrome plugin config changes

---
+++ public/content-script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const app = document.createElement('div');
+    app.id = 'chrome-extension-app-root';
+    document.body.appendChild(app);
+  
+    const script = document.createElement('script');
+    script.src = chrome.runtime.getURL('static/js/main.js');
+    document.head.appendChild(script);
+  });
+  
